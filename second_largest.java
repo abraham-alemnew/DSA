815b81a1eefb88d55e4efa03685473822b26cf83
abraham-alemnew <153062333+abraham-alemnew@users.noreply.github.com>
2024-01-15 23:38:58 -0800

    Update second_largest.java

--- second_largest.java
+++ second_largest.java
@@ -21,29 +21,27 @@ public class second_largest {
 	public static String find_second_largest(int[] n) {
 
 		int max = n[0];
-		// secondLargest will always be at least -1 if array is not empty and there are
-		// no
-		// negative values
-		// - no negatives -- create repo
+		
+		
 
 		double secondLargest = 0.5;
 
-		// This loop will promote the max is there is a new max and then create the
-		// second largest variable
+		// This loop will promote the max until it reaches the end of array
 		for (int i = 1; i < n.length; i++) {
 			if (max < n[i]) {
 
 				max = n[i];
 			}
 		}
+		// Checking for a second largest number if available
 		for (int i = 0; i < n.length; i++) {
 			if (n[i] < max && n[i] > secondLargest) {
 
 				secondLargest = n[i];
 			}
 		}
-		// The three cases is an empty array, all the same numbers, and a second largest
-		// value available
+		// The two cases are all the same numbers or the second largest value available 
+		
 		if (secondLargest == 0.5)
 			return "No second largest number";
 
