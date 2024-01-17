a49f455f8af8076851e744f0ebad6cb0bae2a7c9
abraham-alemnew <153062333+abraham-alemnew@users.noreply.github.com>
2024-01-16 21:18:28 -0800

    Update second_largest.java

--- second_largest.java
+++ second_largest.java
@@ -3,20 +3,17 @@
 package dsa;
 import java.util.Collection;
 
-
 public class second_largest {
 
 	public static void main(String[] args) {
 		int[] test1 = { 5, 2, 8, 1, 9, 3 };
 
 		int[] test2 = { 4, 4, 4, 4, 4, 4 };
-		
+
 		int[] test3 = { 10, 5, 10, 20, 20, 15 };
 
-
 		int[] test4 = { -10, -5, -10, -20, -20, -15 };
-		
-		
+
 		System.out.println(find_second_largest(test1));
 		System.out.println(find_second_largest(test2));
 		System.out.println(find_second_largest(test3));
@@ -26,8 +23,6 @@ public class second_largest {
 	public static String find_second_largest(int[] n) {
 
 		int max = Integer.MIN_VALUE;
-		
-		
 
 		double secondLargest = Integer.MIN_VALUE;
 
@@ -37,16 +32,13 @@ public class second_largest {
 
 				max = n[i];
 			}
-		}
-		// Checking for a second largest number if available
-		for (int i = 0; i < n.length; i++) {
 			if (n[i] < max && n[i] > secondLargest) {
 
 				secondLargest = n[i];
 			}
 		}
-		// The two cases are all the same numbers or the second largest value available 
-		
+		// Checking for a second largest number if available
+
 		if (secondLargest == Integer.MIN_VALUE)
 			return "No second largest number";
 
@@ -56,3 +48,4 @@ public class second_largest {
 	}
 
 }
+
